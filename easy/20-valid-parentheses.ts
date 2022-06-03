08b64482736efcaad2b38ccedb8fdde59fe9458d
David Glymph <Woozl@users.noreply.github.com>
2022-06-03 10:23:05 -0400

    init & valid parentheses

---
+++ easy/20-valid-parentheses.ts
@@ -0,0 +1,23 @@
+function isValid(s: string): boolean {
+    let pairs = {
+        ']': '[',
+        '}': '{',
+        ')': '('
+    };
+    
+    let stack = [];
+    
+    for(let c of s) {
+        if(Object.keys(pairs).includes(c)) { // ], }, )
+            if(stack.length === 0) return false; // s = "}"
+            
+            if(stack.pop() !== pairs[c]) return false; // s = "[}"     
+        } else {
+            stack.push(c);
+        }        
+    }
+    
+    if(stack.length !== 0) return false; // s = "()("
+    
+    return true;
+};
