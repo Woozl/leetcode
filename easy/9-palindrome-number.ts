e1f2bd8f8b331f1ab23d6a9376b538563a723c72
David Glymph <Woozl@users.noreply.github.com>
2022-06-04 12:22:13 -0400

    palindrome number

---
+++ easy/9-palindrome-number.ts
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/palindrome-number/
+
+function isPalindrome(x: number): boolean {
+    if(x < 0) return false;
+
+    const stringified = x.toString();
+    const length = stringified.length;
+
+    for(let i = 0; i <= length/2; ++i) {
+        if(stringified[i] !== stringified[length - 1 - i]) return false;
+    }
+
+    return true;
+};
