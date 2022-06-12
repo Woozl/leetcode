fc3133e08ea03bb2dca4e4ff3370662a96622ce6
David Glymph <Woozl@users.noreply.github.com>
2022-06-12 09:00:48 -0400

    two sum

---
+++ easy/1-two-sum.ts
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/two-sum/
+
+function twoSum(nums: number[], target: number): number[] {
+    let vals: any = {};
+    let out: number[] = []
+    nums.forEach((v, i) => {
+        let eq = target - v;
+        if(vals[eq] !== undefined){
+            out = [vals[eq], i];
+            return;
+        }
+        else
+            vals[v] = i;
+    });
+    return out;
+}
+
+// function twoSum(nums: number[], target: number): number[] {
+//     for(let i = 0; i < nums.length; ++i) {
+//         let firstNum = nums[i];
+//         for(let j = i + 1; j < nums.length; ++j) {
+//             if(firstNum + nums[j] === target) return [i, j];
+//         }
+//     }
+// };
+
+
+console.log(twoSum([2,7,11,15],9));
