8784626649e07829a98b82a58eb88ac2bad7f5c0
David Glymph <Woozl@users.noreply.github.com>
2022-06-12 11:08:24 -0400

    valid palindrome

---
+++ easy/125-valid-palindrome.ts
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+function isPalindrome(s: string): boolean {
+    s = s.toLowerCase().replace(/[^a-z0-9]*/g, '');
+    console.log(s);
+
+    for(let i = 0; i < s.length / 2; ++i) {
+        if(s[i] !== s[s.length - i - 1]) return false;
+    }
+    
+    return true;
+};
+
+// console.log(isPalindrome("A man, a plan, a canal, Panama!"));
