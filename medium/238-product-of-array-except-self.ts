db5c7351c799ad5d9d10ad89acf2eb2c5ddffa14
David Glymph <Woozl@users.noreply.github.com>
2022-06-29 14:47:15 -0400

    O(1) space solution

--- medium/238-product-of-array-except-self.ts
+++ medium/238-product-of-array-except-self.ts
@@ -1,30 +1,54 @@
 // https://leetcode.com/problems/product-of-array-except-self/
 
+// Time: O(n), Space: O(1) -- output array isn't counted
 function productExceptSelf(nums: number[]): number[] {
-    let prods = new Array<number>(nums.length);
-    for(let i = prods.length - 1; i >= 0; --i)
-        prods[i] = (i<prods.length-1 ? nums[i]*prods[i+1] : nums[i])
+    let out = new Array<number>(nums.length);
 
-    console.log(prods);
-
-    const list = new Array<number>(nums.length);
-
-    let lastProd = 1;
-    for(let i = 0; i < list.length; ++i) {
-        console.log(lastProd);
-
-        if(i + 1 < list.length)
-            list[i] = lastProd * prods[i + 1];
-        else
-            list[i] = lastProd;
-
-        lastProd *= nums[i];
+    out[0] = 1;
+    for(let i = 1; i < nums.length; ++i) {
+        out[i] = nums[i - 1] * out[i - 1];
     }
 
-    return list;
-};
+    let fac = 1;
+    for(let i = nums.length - 1; i >= 0; --i) {
+        out[i] = out[i] * fac;
+        fac = fac * nums[i];
+    }
 
-// console.log(productExceptSelf([2, 3]));
+    return out;
+}
+
+// 1   2   3   4
+// 24  12  8   6
+// 1   2   6   24
+//             
+
+// Time: O(1) Space: O(n)
+// function productExceptSelf(nums: number[]): number[] {
+//     let prods = new Array<number>(nums.length);
+//     for(let i = prods.length - 1; i >= 0; --i)
+//         prods[i] = (i<prods.length-1 ? nums[i]*prods[i+1] : nums[i])
+
+//     console.log(prods);
+
+//     const list = new Array<number>(nums.length);
+
+//     let lastProd = 1;
+//     for(let i = 0; i < list.length; ++i) {
+//         console.log(lastProd);
+
+//         if(i + 1 < list.length)
+//             list[i] = lastProd * prods[i + 1];
+//         else
+//             list[i] = lastProd;
+
+//         lastProd *= nums[i];
+//     }
+
+//     return list;
+// };
+
+console.log(productExceptSelf([1, 2, 3, 4]));
 // solution: 120 60 40 30 24
 
 // 120 120 60 20 5      R->L product (precalc)
