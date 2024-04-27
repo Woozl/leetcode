6f5c146393d8014d4467cf11fba655b746e7c102
David Glymph <Woozl@users.noreply.github.com>
2024-04-27 14:51:27 -0400

    contains duplicate

--- easy/217-contains-duplicate.ts
+++ easy/217-contains-duplicate.ts
@@ -1,12 +1,28 @@
 // https://leetcode.com/problems/contains-duplicate/
 
+// Time: O(nlogn), Space: O(logn)
 function containsDuplicate(nums: number[]): boolean {
-    const s = new Set<number>();
+    nums.sort();
+    for (let i = 0; i < nums.length; ++i) {
+        const x = nums[i];
+        const y = nums[i + 1];
 
-    for(const num of nums) {
-        if(s.has(num)) return true;
-        s.add(num);
+        if (y === undefined) return false;
+
+        if (x === y) return true;
     }
-
+    
     return false;
 };
+
+// Time: O(n), Space: O(n):
+// function containsDuplicate(nums: number[]): boolean {
+//     const s = new Set<number>();
+
+//     for(const num of nums) {
+//         if(s.has(num)) return true;
+//         s.add(num);
+//     }
+
+//     return false;
+// };
